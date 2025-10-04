cb05c112209d65e0417daae09e41c1349778e74b
chumachenkoUA <kirito22122005@gmail.com>
2025-10-04 04:02:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+
+inputs.forEach((input) => {
+  const lable = document.createElement('label');
+
+  lable.classList.add('field-label');
+  lable.classList.add('for');
+  lable.textContent = input.name;
+  lable.setAttribute('for', input.id);
+  input.before(lable);
+
+  const inputName = input.name;
+
+  input.setAttribute(
+    'placeholder',
+    inputName[0].toUpperCase() + inputName.slice(1),
+  );
+});
